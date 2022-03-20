67566a33eb8797270965ab8a3f85b2e008395a7c
Myriam Delbreil <myriam.delbreil@live.fr>
2022-03-20 19:38:21 +0100

    seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+# 10.times do
+#   email = Faker::Internet.email
+#   username = Faker::Internet.username
+#   password = "123456"
+#   user = User.create(email: email, username: username, password: password)
+# end
+
+# puts "Users created. Now articles"
+
+# User.all.each do |user|
+#   title = Faker::Book.title
+#   content = "#{Faker::Lorem.paragraph_by_chars} #{Faker::Lorem.paragraph_by_chars}"
+#   article = Article.create(user: user, content: content, title: title)
+# end
+
+# puts "Articles created. Now reviews"
+
+Article.all.each do |article|
+  user = User.all.sample
+  title = Faker::Lorem.word
+  content = Faker::Lorem.sentence
+  review = Review.create(title: title, content: content, user: user, article: article)
+end
+
+puts "reviews created!"
