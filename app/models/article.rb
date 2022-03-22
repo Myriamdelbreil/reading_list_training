9175e59415d016e0fa63b4cd0a93779fd151db96
Myriam Delbreil <myriam.delbreil@live.fr>
2022-03-22 19:01:02 +0100

    ok

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_one_attached :photo
   validates :description, length: {maximum: 100}
 end
